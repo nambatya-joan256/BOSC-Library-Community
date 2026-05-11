f5a9831f8a47b1874cccc8dbdf514d4b31f003a5
nambatya-joan256 <150013427+nambatya-joan256@users.noreply.github.com>
2026-05-11 19:14:29 +0000

    fix:added missing 5G protocol to resource index

--- src/wireless_manager.py
+++ src/wireless_manager.py
@@ -1,10 +1,12 @@
 def calculate_signal_strength(distance, power):
-    # BUG: This should be power / (distance ** 2) but it's currently addition
-    'return power / (distance ** 2)' 
+    # This was fixed in Issue #1
+    return power / (distance ** 2) 
 
 def get_protocol_info(protocol):
+    # This is being fixed in Issue #2
     protocols = {
         "WiFi": "802.11ax",
-        "LoRa": "Low Power Wide Area"
+        "LoRa": "Low Power Wide Area",
+        "5G": "3GPP Release 16"  # We added this line!
     }
     return protocols.get(protocol, "Unknown Protocol")
